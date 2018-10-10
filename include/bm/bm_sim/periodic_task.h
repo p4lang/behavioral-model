f8f580f3afa2ce3ad243bb0e03c3c9ccbd62ecaa
Isaac Pedisich <iped@seas.upenn.edu>
2018-10-10 12:43:00 -0400

    added macros to register periodic externs

--- include/bm/bm_sim/periodic_task.h
+++ include/bm/bm_sim/periodic_task.h
@@ -42,6 +42,17 @@ struct PeriodicTask {
   ~PeriodicTask();
 };
 
+// This smells a bit (binding 'this' in inline member initialization),
+// but it compiles and I haven't found documentation against it
+#define REGISTER_EXTERN_PERIODIC(extrn_name, fn, interval) \
+  PeriodicTask fn## __task = \
+      PeriodicTask(#extrn_name #fn, \
+                   std::bind(&extrn_name::fn, this), \
+                   interval);
+
+#define REGISTER_GLOBAL_PERIODIC(fn, interval) \
+  static PeriodicTask fn## __task = PeriodicTask(#fn, fn, interval);
+
 class PeriodicTaskList {
   std::mutex queue_mutex;
   std::condition_variable cv;
