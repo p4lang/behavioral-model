8d0ba28fb8a3f41cef843dedd9ad8c8508e92561
derekso1 <dts76@cornell.edu>
2019-03-23 19:35:01 +0000

    Fix metadata population in receive_

--- targets/psa_switch/psa_switch.cpp
+++ targets/psa_switch/psa_switch.cpp
@@ -149,14 +149,15 @@ PsaSwitch::receive_(port_t port_num, const char *buffer, int len) {
   phv->reset_metadata();
 
   // setting standard metadata
+
   phv->get_field("psa_ingress_parser_input_metadata.ingress_port").set(port_num);
   phv->get_field("psa_ingress_parser_input_metadata.packet_path").set(PKT_INSTANCE_TYPE_NORMAL);
   // using packet register 0 to store length, this register will be updated for
   // each add_header / remove_header primitive call
-  //packet->set_register(PACKET_LENGTH_REG_IDX, len);
-  //phv->get_field("standard_metadata.packet_length").set(len);
-  //Field &f_instance_type = phv->get_field("standard_metadata.instance_type");
-  //f_instance_type.set(PKT_INSTANCE_TYPE_NORMAL);
+//   packet->set_register(PACKET_LENGTH_REG_IDX, len);
+//   phv->get_field("standard_metadata.packet_length").set(len);
+//   Field &f_instance_type = phv->get_field("standard_metadata.instance_type");
+//   f_instance_type.set(PKT_INSTANCE_TYPE_NORMAL);
 
   if (phv->has_field("intrinsic_metadata.ingress_global_timestamp")) {
     phv->get_field("intrinsic_metadata.ingress_global_timestamp")
@@ -327,14 +328,12 @@ PsaSwitch::check_queueing_metadata() {
 void
 PsaSwitch::ingress_thread() {
   PHV *phv;
+
   while (1) {
     std::unique_ptr<Packet> packet;
     input_buffer.pop_back(&packet);
-
     if (packet == nullptr) break;
-    
 
-    // TODO(antonin): only update these if swapping actually happened?
     Parser *parser = this->get_parser("ingress_parser");
     Pipeline *ingress_mau = this->get_pipeline("ingress");
 
