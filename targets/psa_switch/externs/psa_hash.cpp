d8f0a43f47361ec423c879a3113ee7a12d72e864
Antonin Bas <abas@vmware.com>
2021-09-22 10:10:01 -0700

    [psa_switch] Pre-allocate buffer in PSAHash extern implementation

--- targets/psa_switch/externs/psa_hash.cpp
+++ targets/psa_switch/externs/psa_hash.cpp
@@ -17,7 +17,6 @@
  *
  */
 
-
 #include "psa_hash.h"
 
 namespace {
@@ -26,18 +25,16 @@ bm::ByteContainer
 build_buffer(const std::vector<bm::Field> &fields) {
   int nbits = 0;
   int nbytes;
-  bm::ByteContainer buf;
   for (const auto &field : fields) {
     nbits += field.get_nbits();
   }
   nbytes = (nbits + 7) / 8;
+  bm::ByteContainer buf(nbytes, '\x00');
   nbits = (nbytes * 8 - nbits);  // pad to the left with 0s
   for (const auto &field : fields) {
-    int nbits_ = nbits + field.get_nbits();
-    buf.resize((nbits_ + 7) / 8, '\x00');
     char *ptr = buf.data() + (nbits / 8);
     field.deparse(ptr, nbits % 8);
-    nbits = nbits_;
+    nbits += field.get_nbits();
   }
   return buf;
 }
@@ -50,29 +47,27 @@ namespace psa {
 
 void
 PSA_Hash::init() {
-  this->calc = CalculationsMap::get_instance()->get_copy(algo);
+  calc = CalculationsMap::get_instance()->get_copy(algo);
 }
 
 void
 PSA_Hash::get_hash(Field &dst, const std::vector<Field> &fields) {
-  uint64_t hash;
-  ByteContainer buf = build_buffer(fields);
-  hash = compute(buf.data(), buf.size());
+  auto buf = build_buffer(fields);
+  auto hash = compute(buf.data(), buf.size());
   dst.set(hash);
 }
 
 void
 PSA_Hash::get_hash_mod(Field &dst, const Data &base, const std::vector<Field> &fields, const Data &max) {
-  uint64_t hash;
-  ByteContainer buf = build_buffer(fields);
-  hash = compute(buf.data(), buf.size());
-  uint64_t result = base.get<uint64_t>() + (hash % max.get<uint64_t>());
+  auto buf = build_buffer(fields);
+  auto hash = compute(buf.data(), buf.size());
+  auto result = base.get<uint64_t>() + (hash % max.get<uint64_t>());
   dst.set(result);
 }
 
 uint64_t
 PSA_Hash::compute(const char *buf, size_t s) {
-  return this->calc.get()->output(buf, s);
+  return calc.get()->output(buf, s);
 }
 
 BM_REGISTER_EXTERN_W_NAME(Hash, PSA_Hash);
