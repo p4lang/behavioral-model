9bb166cc5f0cf606d21b76b2524ed84fa0849fa3
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2022-03-22 22:32:56 +0000

    Enable P4Runtime support for psa_switch
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- targets/psa_switch/main.cpp
+++ targets/psa_switch/main.cpp
@@ -22,6 +22,7 @@
 
 #include <bm/PsaSwitch.h>
 #include <bm/bm_runtime/bm_runtime.h>
+#include <bm/bm_sim/options_parse.h>
 #include <bm/bm_sim/target_parser.h>
 
 #include <bm/grpc/pem.h>
@@ -30,11 +31,7 @@
 #include <iostream>
 
 #include "psa_switch.h"
-
-namespace {
-bm::psa::PsaSwitch *psa_switch;
-bm::TargetParserBasic *psa_switch_parser;
-}  // namespace
+#include "switch_runner.h"
 
 namespace pswitch_runtime {
 shared_ptr<PsaSwitchIf> get_handler(bm::psa::PsaSwitch *sw);
@@ -43,19 +40,17 @@ shared_ptr<PsaSwitchIf> get_handler(bm::psa::PsaSwitch *sw);
 int
 main(int argc, char* argv[]) {
   using bm::psa::PsaSwitch;
-  psa_switch = new PsaSwitch();
-  psa_switch_parser = new bm::TargetParserBasic();
-  psa_switch_parser->add_flag_option(
-    "enable-swap",
-    "enable JSON swapping at runtime"
-  );
-  psa_switch_parser->add_uint_option(
+
+  bm::TargetParserBasicWithDynModules psa_switch_parser;
+  psa_switch_parser.add_flag_option(
+    "disable-swap",
+    "enable JSON swapping at runtime");
+  psa_switch_parser.add_uint_option(
     "drop-port",
     "Choose a numerical value for the drop port (default is 511). "
     "You will need to use this command-line option when you wish to use port "
-    "511 as a valid dataplane port or as the CPU port."
-  );
-  psa_switch_parser->add_uint_option(
+    "511 as a valid dataplane port or as the CPU port.");
+  psa_switch_parser.add_uint_option(
     "cpu-port",
     "Choose a numerical value for the CPU port, it will be used for "
     "packet-in / packet-out. Do not add an interface with this port number, "
@@ -63,125 +58,110 @@ main(int argc, char* argv[]) {
     "If you do not use this command-line option, "
     "P4Runtime packet IO functionality will not be available: you will not "
     "be able to receive / send packets using the P4Runtime StreamChannel "
-    "bi-directional stream."
-  );
-  psa_switch_parser->add_string_option(
+    "bi-directional stream.");
+  psa_switch_parser.add_string_option(
+      "dp-grpc-server-addr",
+      "Use a gRPC channel to inject and receive dataplane packets; "
+      "bind this gRPC server to given address, e.g. 0.0.0.0:50052");
+  psa_switch_parser.add_string_option(
     "grpc-server-addr",
-    "Bind gRPC server to given address [default is 0.0.0.0:9559]"
-  );
-  psa_switch_parser->add_flag_option(
+    "Bind gRPC server to given address [default is 0.0.0.0:9559]");
+  psa_switch_parser.add_flag_option(
     "grpc-server-ssl",
-    "Enable SSL/TLS for gRPC server"
-  );
-  psa_switch_parser->add_string_option(
+    "Enable SSL/TLS for gRPC server");
+  psa_switch_parser.add_string_option(
     "grpc-server-cacert",
-    "Path to pem file holding CA certificate to verify peer against"
-  );
-  psa_switch_parser->add_string_option(
+    "Path to pem file holding CA certificate to verify peer against");
+  psa_switch_parser.add_string_option(
     "grpc-server-cert",
-    "Path to pem file holding server certificate"
-  );
-  psa_switch_parser->add_string_option(
+    "Path to pem file holding server certificate");
+  psa_switch_parser.add_string_option(
     "grpc-server-key",
-    "Path to pem file holding server key"
-  );
-  psa_switch_parser->add_flag_option(
+    "Path to pem file holding server key");
+  psa_switch_parser.add_flag_option(
     "grpc-server-with-client-auth",
-    "Require client to have a valid certificate for mutual authentication"
-  );
+    "Require client to have a valid certificate for mutual authentication");
 
-  int status = psa_switch->init_from_command_line_options(
-      argc, argv, psa_switch_parser);
-  if (status != 0) std::exit(status);
-
-  bool enable_swap_flag = false;
-  if (psa_switch_parser->get_flag_option("enable-swap", &enable_swap_flag)
-      != bm::TargetParserBasic::ReturnCode::SUCCESS)
-    std::exit(1);
-  if (enable_swap_flag) psa_switch->enable_config_swap();
-
-  uint32_t drop_port = 0xffffffff;
-  {
-    auto rc = psa_switch_parser->get_uint_option("drop-port", &drop_port);
-    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED)
-      drop_port = PsaSwitch::default_drop_port;
-    else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS)
-      std::exit(1);
-    psa_switch->set_drop_port(drop_port);
-  }
-
-  uint32_t cpu_port = 0xffffffff;
-  {
-    auto rc = psa_switch_parser->get_uint_option("cpu-port", &cpu_port);
-    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED)
-      cpu_port = 0;
-    else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS || cpu_port == 0)
-      std::exit(1);
-  }
+  bm::OptionsParser parser;
+  parser.parse(argc, argv, &psa_switch_parser);
 
   std::string dp_grpc_server_addr;
   {
-    auto rc = psa_switch_parser->get_string_option(
+    auto rc = psa_switch_parser.get_string_option(
         "dp-grpc-server-addr", &dp_grpc_server_addr);
     if (rc != bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED &&
-        rc != bm::TargetParserBasic::ReturnCode::SUCCESS)
-      std::exit(1);
+        rc != bm::TargetParserBasic::ReturnCode::SUCCESS) {
+        std::cerr << "Failed to parse --dp-grpc-server-addr\n";
+        std::exit(1);
+      }
   }
 
   std::string grpc_server_addr;
   {
-    auto rc = psa_switch_parser->get_string_option(
+    auto rc = psa_switch_parser.get_string_option(
         "grpc-server-addr", &grpc_server_addr);
-    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED)
+    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED) {
       grpc_server_addr = "0.0.0.0:9559";
-    else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS)
+    } else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS) {
+      std::cerr << "Failed to parse --grpc-server-addr\n";
       std::exit(1);
+    }
   }
 
   bool grpc_server_ssl = false;
   {
-    auto rc = psa_switch_parser->get_flag_option(
+    auto rc = psa_switch_parser.get_flag_option(
         "grpc-server-ssl", &grpc_server_ssl);
-    if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS)
+    if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS) {
+      std::cerr << "Failed to parse --grpc-server-ssl\n";
       std::exit(1);
+    }
   }
 
   std::string grpc_server_cacert;
   {
-    auto rc = psa_switch_parser->get_string_option(
+    auto rc = psa_switch_parser.get_string_option(
         "grpc-server-cacert", &grpc_server_cacert);
-    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED)
+    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED) {
       grpc_server_cacert = "";
-    else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS)
+    } else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS) {
+      std::cerr << "Failed to parse --grpc-server-cacert\n";
       std::exit(1);
+    }
   }
 
   std::string grpc_server_cert;
   {
-    auto rc = psa_switch_parser->get_string_option(
+    auto rc = psa_switch_parser.get_string_option(
         "grpc-server-cert", &grpc_server_cert);
-    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED)
+    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED) {
       grpc_server_cert = "";
-    else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS)
+    } else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS) {
+      std::cerr << "Failed to parse --grpc-server-cert\n";
       std::exit(1);
+    }
   }
 
   std::string grpc_server_key;
   {
-    auto rc = psa_switch_parser->get_string_option(
+    auto rc = psa_switch_parser.get_string_option(
         "grpc-server-key", &grpc_server_key);
-    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED)
+    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED) {
       grpc_server_key = "";
-    else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS)
+    } else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS) {
+      std::cerr << "Failed to parse --grpc-server-key\n";
       std::exit(1);
+    }
   }
 
   bool grpc_server_with_client_auth = false;
   {
-    auto rc = psa_switch_parser->get_flag_option(
+    auto rc = psa_switch_parser.get_flag_option(
         "grpc-server-with-client-auth", &grpc_server_with_client_auth);
-    if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS)
+    if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS) {
+      std::cerr << "Failed to parse --grpc-server-with-client-auth\n";
       std::exit(1);
+    }
   }
 
   if (!grpc_server_ssl &&
@@ -189,22 +169,22 @@ main(int argc, char* argv[]) {
        grpc_server_cert != "" ||
        grpc_server_key != "")) {
     std::cerr << "SSL/TLS is disabled for gRPC server, "
-        << "so provided .pem files will be ignored\n";
+              << "so provided .pem files will be ignored\n";
   }
 
   if (!grpc_server_ssl && grpc_server_with_client_auth) {
     std::cerr << "SSL/TLS is disabled for gRPC server, "
-        << "so cannot request client auth\n";
+              << "so cannot request client auth\n";
   }
 
   if (grpc_server_ssl && grpc_server_cert == "") {
     std::cerr << "When enabling SSL/TLS for gRPC server, "
-        << "--grpc-server-cert is required\n";
+              << "--grpc-server-cert is required\n";
     std::exit(1);
   }
   if (grpc_server_ssl && grpc_server_key == "") {
     std::cerr << "When enabling SSL/TLS for gRPC server, "
-        << "--grpc-server-key is required\n";
+              << "--grpc-server-key is required\n";
     std::exit(1);
   }
 
@@ -227,15 +207,58 @@ main(int argc, char* argv[]) {
     std::exit(1);
   }
 
-  int thrift_port = psa_switch->get_runtime_port();
-  bm_runtime::start_server(psa_switch, thrift_port);
+  uint32_t cpu_port = 0xffffffff;
+  {
+    auto rc = psa_switch_parser.get_uint_option("cpu-port", &cpu_port);
+    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED) {
+      cpu_port = 0;
+    } else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS || cpu_port == 0) {
+      std::cerr << "Failed to parse --cpu-port\n";
+      std::exit(1);
+    }
+  }
+
+  std::shared_ptr<PsaSwitch> psa_switch = std::make_shared<PsaSwitch>();
+
+  bool disable_swap_flag = false;
+  {
+    auto rc = psa_switch_parser.get_flag_option("disable-swap", &disable_swap_flag);
+    if (rc  != bm::TargetParserBasic::ReturnCode::SUCCESS) {
+      std::cerr << "Failed to parse --disable-swap\n";
+      std::exit(1);
+    }
+  }
+  if (!disable_swap_flag) psa_switch->enable_config_swap();
+
+  uint32_t drop_port = 0xffffffff;
+  {
+    auto rc = psa_switch_parser.get_uint_option("drop-port", &drop_port);
+    if (rc == bm::TargetParserBasic::ReturnCode::OPTION_NOT_PROVIDED) {
+      drop_port = PsaSwitch::default_drop_port;
+    } else if (rc != bm::TargetParserBasic::ReturnCode::SUCCESS) {
+      std::cerr << "Failed to parse --drop-port\n";
+      std::exit(1);
+    }
+    psa_switch->set_drop_port(drop_port);
+  }
+
+  // Start Thrift server
+  bm_runtime::start_server(psa_switch.get(), parser.thrift_port);
   using ::pswitch_runtime::PsaSwitchIf;
   using ::pswitch_runtime::PsaSwitchProcessor;
   bm_runtime::add_service<PsaSwitchIf, PsaSwitchProcessor>(
-      "psa_switch", pswitch_runtime::get_handler(psa_switch));
-  psa_switch->start_and_return();
+      "psa_switch", pswitch_runtime::get_handler(psa_switch.get()));
 
-  while (true) std::this_thread::sleep_for(std::chrono::seconds(100));
+  auto &runner = switch_runner::SwitchGrpcRunner::get_instance(
+      psa_switch,
+      grpc_server_addr,
+      cpu_port,
+      dp_grpc_server_addr,
+      grpc_server_ssl ? ssl_options : nullptr);
 
+  int status = runner.init_and_start(parser);
+  if (status != 0) std::exit(status);
+
+  runner.wait();
   return 0;
 }
