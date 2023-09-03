a854252cf2fc9836baf5cd0421d3e68317e52b27
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2023-09-03 11:28:11 -0400

    Install a more recent version of nnpy

--- ci/install-nnpy.sh
+++ ci/install-nnpy.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 set -e
-git clone https://github.com/nanomsg/nnpy.git
-# top of tree won't install
-cd nnpy
-git checkout c7e718a5173447c85182dc45f99e2abcf9cd4065
-sudo pip3 install cffi
-sudo pip3 install .
-cd ..
+sudo pip3 install nnpy cffi
