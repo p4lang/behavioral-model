ac3f42bbf28b5fecff324cf6071825fe6f8e54ce
fruffy <fruffy@nyu.edu>
2026-03-29 15:25:47 -0400

    Replace nnpy with pynng.
    Signed-off-by: fruffy <fruffy@nyu.edu>

---
+++ ci/check-pynng.py
@@ -0,0 +1,28 @@
+import sys
+import time
+
+import pynng
+
+address = "inproc://foo"
+
+pub = pynng.Pub0(listen=address)
+sub = pynng.Sub0(dial=address, recv_timeout=500)
+sub.subscribe("")
+
+ok = False
+for _ in range(5):
+    pub.send(b"hello, world")
+    try:
+        recv = sub.recv()
+    except pynng.Timeout:
+        time.sleep(0.05)
+        continue
+    if recv == b"hello, world":
+        ok = True
+        break
+
+pub.close()
+sub.close()
+
+if not ok:
+    sys.exit(1)
