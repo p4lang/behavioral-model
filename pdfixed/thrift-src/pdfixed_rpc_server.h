0a70678af697af8eac390f093968d7f4d1b009e5
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-04-22 21:02:03 +0000

    Add Apache-2.0 license to the last few C++ source files
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- pdfixed/thrift-src/pdfixed_rpc_server.h
+++ pdfixed/thrift-src/pdfixed_rpc_server.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2016 Barefoot Networks, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #ifndef _P4_PDFIXED_RPC_SERVER_H_
 #define _P4_PDFIXED_RPC_SERVER_H_
 
