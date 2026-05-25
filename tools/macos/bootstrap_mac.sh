6c7f289b9f504ada7d082def90f0e38d44fd257b
Devansh-567 <devansh.jay.singh@gmail.com>
2026-05-25 17:24:03 +0530

    fix: update bootstrap_mac.sh with modern Homebrew installer and dependencies
    Signed-off-by: Devansh-567 <devansh.jay.singh@gmail.com>

--- tools/macos/bootstrap_mac.sh
+++ tools/macos/bootstrap_mac.sh
@@ -7,58 +7,58 @@
 set -e
 set -x
 
-tools_dir=`dirname $0`
+# Installation helper.
+brew_install() {
+    echo "\nInstalling $1"
+    if brew list $1 &>/dev/null; then
+        echo "${1} is already installed"
+    else
+        brew install --ignore-dependencies $1 && echo "$1 is installed"
+    fi
+}
 
-# check for brew,
-if [[ -z `which brew` ]]; then
-    # install homebrew
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    # python pip installs in /usr/local/tests
-    sudo mkdir -p /usr/local/tests
-    sudo chown ${USER}:admin /usr/local/tests
+# Check if Homebrew is already installed.
+if ! which brew > /dev/null 2>&1; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# check for pip
-if [[ -z `which pip` ]]; then
-    (>&2 echo 'Please install pip by "curl -O https://bootstrap.pypa.io/get-pip.py | sudo python3 get-pip.py"')
-    exit 1
+# Check if brew shellenv command is already in zprofile.
+if ! grep -q 'brew shellenv' ~/.zprofile; then
+    if [[ $(uname -m) == 'arm64' ]]; then
+        (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    else
+        (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.zprofile
+        eval "$(/usr/local/bin/brew shellenv)"
+    fi
 fi
 
-# check for clang version
-cxx_full_version=`clang++ --version | grep version`
-cxx_version=`echo $cxx_full_version | cut -f 4 -d " "`
-if [[ $cxx_version < "8.0.0" ]]; then
-    echo "Please install Xcode 8 or command line tools v8 or higher."
-    echo "It is a requirement for thread_local support"
-    echo ""
-    echo "You have: $cxx_full_version"
-    exit 1
-fi
+# Source zprofile.
+source ~/.zprofile
 
+HOMEBREW_PREFIX=$(brew --prefix)
+
+# Fetch the latest formulae.
 brew update
 
-# install basic tools
-brew install automake autoconf bison boost clang-format cmake coreutils \
-     doxygen gcc@5 gmp libevent openssl pkg-config wget
-# bison needs a bit more nudging
-brew link --overwrite --force bison
+REQUIRED_PACKAGES=(
+    autoconf automake cmake libtool
+    boost bison pkg-config
+    libevent openssl coreutils
+    nanomsg thrift
+    python3
+)
 
-# we need realpath from coreutils
-brews_dir=`realpath $tools_dir`/brews
+for package in "${REQUIRED_PACKAGES[@]}"; do
+    brew_install ${package}
+done
 
-# thrift
-brew install --HEAD ${brews_dir}/thrift.rb --with-python --with-libevent
+# bison needs to be on PATH before system bison.
+if ! grep -q "$(brew --prefix bison)/bin" ~/.bash_profile; then
+    echo 'export PATH="$(brew --prefix bison)/bin:$PATH"' >> ~/.bash_profile
+fi
 
-brew install python
-/usr/local/bin/pip install pcapy
+source ~/.bash_profile
 
-# nanomsg
-brew install nanomsg
-
-# # scapy
-pip install scapy
-
-# pynng
-/usr/local/bin/pip install pynng==0.9.0
-
-/usr/local/bin/pip install PyYAML
+# Install Python dependencies.
+pip3 install scapy pynng==0.9.0 PyYAML
